ed555044b693c305eeda4988ca27a04f563e323b
k-hara <k.hara.pg+dev@gmail.com>
2010-06-09 12:18:08 +0900

    cleanup canon code.

--- canon.d
+++ canon.d
@@ -1,12 +1,8 @@
 ﻿module canon;
 
-static import temp;
-import T = tree;
-import tree;
-import typecons.tuple_match;
-import typecons.tuple_tie;
+import temp, tree;
+import typecons.tuple_match, typecons.tuple_tie;
 import std.traits;
-import std.typetuple;
 
 import debugs;
 
@@ -15,7 +11,7 @@ Stm[] linearize(Stm s)
 {
 	Stm[] linear(Stm s, Stm[] l){
 		Stm[] sl;
-		if( T.SEQ(&sl) = s ){
+		if( SEQ(&sl) = s ){
 			return linear(sl[0],linear(sl[1],l));
 	    }else{
 			return s~l;
@@ -35,9 +31,9 @@ Stm seq(Stm x, Stm y)
 
 bool commute(Stm s, Exp e)
 {
-	if( T.EXP(T.VINT(_)) = s ) return true;
-	if( T.NAME(_) = e ) return true;
-	if( T.VINT(_) = e ) return true;
+	if( EXP(VINT(_)) = s ) return true;
+	if( NAME(_) = e ) return true;
+	if( VINT(_) = e ) return true;
 	else	return false;
 }
 
@@ -51,9 +47,9 @@ Tuple!(Stm, Exp[]) reorder(Exp[] el)
 		auto a = el[0];
 		auto rest = el[1..$];
 		
-		if( T.CALL(_, _) = a ){
+		if( CALL(_, _) = a ){
 			auto t = temp.newTemp();
-			return reorder(T.ESEQ(T.MOVE(T.TEMP(t), a), T.TEMP(t)) ~ rest);
+			return reorder(ESEQ(MOVE(TEMP(t), a), TEMP(t)) ~ rest);
 		}else{
 			Stm s1, s2;
 			Exp e;
@@ -63,106 +59,58 @@ Tuple!(Stm, Exp[]) reorder(Exp[] el)
 				return tuple(seq(s1, s2), e~el);
 			}else{
 				auto t = temp.newTemp();
-				return tuple(seq(seq(s1, T.MOVE(T.TEMP(t), e)), s2), T.TEMP(t) ~ el);
+				return tuple(seq(seq(s1, MOVE(TEMP(t), e)), s2), TEMP(t) ~ el);
 			}
 		}
 	}
 }
 
-/+private template Expand(alias A, size_t N, E...)
+Tuple!(Stm, Exp) reorder_exp(EL, BLD)(EL el_, BLD build)
 {
-	static if( E.length == 0 ){
-		alias TypeTuple!() field;
-	}else static if( is(E[0] U == U[]) ){
-		enum field = Tuple!(A[N..A.length]);
-	}else{
-		alias Tuple!(A[N], Expand!(A, N+1, E[1..$]).field) field;
-	}
-}+/
-
-private template isExp(E){ enum isExp = is(E == Exp); }
-
-Tuple!(Stm, Exp) reorder_exp(EL, TE...)(EL el_, Exp delegate(TE) build)
-  if( is(EL==void[]) || is(EL==Exp[]) )
-in{
-	static if( TE.length == 1 && is(TE[0] == Exp[]) ){
-	}else{
-		static if( is(TE[$-1]==Exp[]) )	static assert(allSatisfy!(isExp, TE[0..$-1]));
-		else							static assert(allSatisfy!(isExp, TE[0..$]));
-	}
-}body{
 	Stm   s;
-	Exp[] el = cast(Exp[])el_;
-	tie(s, el) = reorder(el);
-	
-	//return tuple(s, build(Expand!(el, 0, TE).field));
-	static if( TE.length == 1 ){
-		static if( is(TE[$-1]==Exp[]) )	return tuple(s, build(el[0..$]));
-		else							return tuple(s, build(el[0]));
-	}
-	static if( TE.length == 2 ){
-		static if( is(TE[$-1]==Exp[]) )	return tuple(s, build(el[0], el[1..$]));
-		else							return tuple(s, build(el[0], el[1]));
-	}
-	static if( TE.length >= 3 ) static assert(0);
+	Exp[] el;
+	tie(s, el) = reorder(cast(Exp[])el_);
+	return tuple(s, callBuild(el, build));
 }
-/+Tuple!(Stm, Exp) reorder_exp(Exp[] xs, Exp delegate(Exp[]) b){
-	return tuple(Stm.init, Exp.init);//todo
-}+/
 
-Stm reorder_stm(EL, TE...)(EL el_, Stm delegate(TE) build)
-  if( is(EL==void[]) || is(EL==Exp[]) )
-in{
-	static if( TE.length == 1 && is(TE[0] == Exp[]) ){
-	}else{
-		static if( is(TE[$-1]==Exp[]) )	static assert(allSatisfy!(isExp, TE[0..$-1]));
-		else							static assert(allSatisfy!(isExp, TE[0..$]));
-	}
-}body{
-	Stm s;
-	Exp[] el = cast(Exp[])el_;
-	tie(s, el) = reorder(el);
-	
-	//return seq(s, build(Expand!(el, 0, TE).field));
-	static if( TE.length == 1 ){
-		static if( is(TE[$-1]==Exp[]) ){
-			return seq(s, build(el[0..$]));
-		}else{
-//			pp("reorder_stm, s={");
-//			debugout(s);
-//			pp("}, el[0]={");
-//			debugout(el[0]);
-//			pp("}");
-			return seq(s, build(el[0]));
-		}
-	}
-	static if( TE.length == 2 ){
-		static if( is(TE[$-1]==Exp[]) )	return seq(s, build(el[0], el[1..$]));
-		else							return seq(s, build(el[0], el[1]));
-	}
-	static if( TE.length >= 3 ) static assert(0);
+Stm reorder_stm(EL, BLD)(EL el_, BLD build)
+{
+	Stm   s;
+	Exp[] el;
+	tie(s, el) = reorder(cast(Exp[])el_);
+	return seq(s, callBuild(el, build));
 }
-/+Stm reorder_stm(Exp[] xs, Stm delegate(Exp[]) b){
-	Exp e;
-	Stm s, s1;
-	foreach( x; xs ){
-		if( ESEQ(&s, &e) = x ){
-		//	tie(s1, e) = reorder_exp([e]);
-		}else{
+
+private{
+	template isExp(E){ enum isExp = is(E == Exp); }
+	R callBuild(R, TE...)(Exp[] el, R delegate(TE) build)
+	{
+		static assert(TE.length>=1);
+		static if( allSatisfy!(isExp, TE[0..$-1], Exp) && is(TE[$-1] == Exp[]) ){
+			static if( TE.length == 1 )	return build(el[0..$]);
+			static if( TE.length == 2 )	return build(el[0], el[1..$]);
+			static if( TE.length >= 3 ) static assert(0);
+		}
+		else static if( allSatisfy!(isExp, TE[0..$]) ){
+			static if( TE.length == 1 )	return build(el[0]);
+			static if( TE.length == 2 )	return build(el[0], el[1]);
+			static if( TE.length >= 3 ) static assert(0);
+		}
+		else{
+			static assert(0);
 		}
 	}
-	return null;//todo
-}+/
+}
 
 Stm do_stm(Stm s)
 {
-	temp.Temp		r;
-	temp.Label		t, f;
-	temp.Label[]	ll;
+	Temp	r;
+	Label	t, f;
+	Label[]	ll;
 	Exp		e, a, b;
 	Exp[]	el;
 	Stm[]	sl;
-	Relop rop;
+	Relop	rop;
 	
 	alias Exp E;
 	
@@ -180,18 +128,7 @@ Stm do_stm(Stm s)
 			return reorder_stm(e~el, (E e, E[] el){ return EXP(CALL(e,el)); });
 		},
 		EXP(&e),{
-//			pp("EXP(&e){");
-//			debugout(e);
-//			pp("}");
-			auto ret = reorder_stm([e], (E e){
-//				pp("EXP(&e).reorder/build{");
-//				debugout(e);
-//				pp("}");
-				return EXP(e); });
-//			pp("EXP(&e), ret={");
-//			debugout(ret);
-//			pp("}");
-			return ret;
+			return reorder_stm([e], (E e){ return EXP(e); });
 		},
 		JUMP(&e, &ll),{
 			return reorder_stm([e], (E e){ return JUMP(e, ll); });
@@ -211,8 +148,8 @@ Stm do_stm(Stm s)
 
 Tuple!(Stm, Exp) do_exp(Exp e)
 {
-	Stm	s;
-	Exp	a, b;
+	Stm		s;
+	Exp		a, b;
 	Exp[]	el;
 	BinOp	op;
 	
@@ -247,14 +184,12 @@ unittest
 	Exp e1_;	Exp e1 = NAME(temp.newLabel());	
 	
 	// 1
-	assert(do_exp(ESEQ(s1, ESEQ(s2, e1)))		== tuple(SEQ([s1, s2]), e1)		);
+	assert(do_exp(ESEQ(s1, ESEQ(s2, e1)))		== tuple(SEQ([s1, s2]), e1));
 	
 	
-	//debugout(do_stm(EXP(ESEQ(s1, ESEQ(s2, e1)))));
-	assert(do_stm(EXP(ESEQ(s1, e1)))			== seq(s1, EXP(e1)));
-	//debugout(do_stm(EXP(ESEQ(s1, ESEQ(s2, e1)))));
-	assert(do_stm(EXP(ESEQ(s1, ESEQ(s2, e1))))	== seq(seq(s1, s2), EXP(e1))	);
+	assert(do_stm(EXP(ESEQ(s1,          e1 )))	== seq(    s1,      EXP(e1)));
+	assert(do_stm(EXP(ESEQ(s1, ESEQ(s2, e1))))	== seq(seq(s1, s2), EXP(e1)));
 	
 	
-	assert(linearize(EXP(ESEQ(s1, ESEQ(s2, e1))))	== [s1, s2, EXP(e1)]	);
+	assert(linearize(EXP(ESEQ(s1, ESEQ(s2, e1))))	== [s1, s2, EXP(e1)]);
 }
